07906e33f126b0352a46f29c87d26305be30ccd8
WilliamBergamin <Berw@videotron.ca>
2019-10-30 13:51:50 -0400

    Need to move dont want to lose anyhting

---
+++ API/app.py
@@ -0,0 +1,62 @@
+import json
+import os
+from flask import request
+from flask_cors import CORS
+from flask import make_response
+from flask_httpauth import HTTPTokenAuth
+
+from init import app, JSON_MIME_TYPE, mongo_db
+
+auth = HTTPTokenAuth('Token')
+
+api_token = os.environ.get('API_TOKEN')
+
+
+@auth.verify_token
+def verify_token(token):
+    # if not int(os.environ.get('ENABLE_FLASK_AUTH')):
+    if not int(0):
+        return True
+    if token == api_token:
+        return True
+    return False
+
+
+@app.route('/', methods=['GET'])
+def test():
+    """
+    Gets informaation about the application.
+    :return: Information about this application.
+    :rtype: str
+    """
+    return 'very AI, many Knowledge, much Cloud'
+
+
+@app.route('/job', methods=['POST'])
+def post_job():
+    """
+    Post a new job for the worker to execute.
+    :param name: name of the model that will execute the job
+    :param images: list of images that must be processed
+    :return: The id of the new job.
+    :rtype: JSON response
+    """
+    if request.content_type != JSON_MIME_TYPE:
+        error = json.dumps({'error': 'Invalid Content Type'})
+        return json_response(error, 400)
+    app.logger.info("Request received for hydra")
+    data = request.json
+    name = data['name']
+    dwarf = Dwarf(name=name, model_host_path=None)
+    res = dwarf.get()
+    if res['state'] != 'up':
+        return auth.login_required(json_response(json.dumps(res), status=404))
+    info = name.split(":")
+    with Connection(REDIS_CONN):
+        job = Queue(name=info[1]).enqueue_call(func="model_wrappers.model_wrapper_manager.run_model_wrapper",
+                                               args=(info[0], data,),
+                                               result_ttl=864000, timeout=600)
+        app.logger.info("Request placed on queue succesfully")
+    return auth.login_required(json_response(json.dumps({
+                                                        "job_id": job.get_id()
+                                                        }), status=201))
---
+++ API/init.py
@@ -0,0 +1,27 @@
+import os
+from pymongo import MongoClient
+from logging.config import dictConfig
+from flask import Flask
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+mongo_db = client['taste_of_heaven']
+
+JSON_MIME_TYPE = 'application/json'
